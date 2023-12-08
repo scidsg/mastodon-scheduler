6b40e58f6897dbf6fce8e0878eb2fbc1d6972ed0
Glenn Sorrentino <git@scidsg.org>
2023-12-07 21:57:24 -0800

    notification animations

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,19 @@ function toggleAltTextField() {
         altTextField.style.display = 'none';
     }
 }
+
+window.onload = function() {
+    // Fade in the notifications
+    var flashMessages = document.querySelectorAll('.notification');
+    flashMessages.forEach(function(msg) {
+        msg.classList.add('notification-visible');
+    });
+
+    // Set timeout to fade out the notifications
+    setTimeout(function() {
+        flashMessages.forEach(function(msg) {
+            msg.classList.remove('notification-visible');
+            msg.classList.add('notification-hidden'); // Start fade-out
+        });
+    }, 5000); // Time before fade-out starts
+};
