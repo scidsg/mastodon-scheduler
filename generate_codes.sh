6d5788b463ea2ee785792bca7806d73c4506bb0c
Glenn Sorrentino <git@scidsg.org>
2023-12-20 12:57:43 -0800

    update generate code script

--- generate_codes.sh
+++ generate_codes.sh
@@ -4,4 +4,26 @@
 python3 -m venv venv
 source venv/bin/activate
 
-python3 /var/www/html/mastodon-scheduler.app/generate_codes.py
+# Enter and test SMTP credentials
+generate_codes() {
+    python3 << END
+from app import app, db, InviteCode
+import secrets
+from datetime import datetime, timedelta
+
+def create_invite_code():
+    code = secrets.token_urlsafe(16)
+    expiration_date = datetime.utcnow() + timedelta(days=365)
+    new_code = InviteCode(code=code, expiration_date=expiration_date)
+    db.session.add(new_code)
+    db.session.commit()
+    return code
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Ensure all tables are created
+        print(create_invite_code())
+END
+}
+
+generate_codes
