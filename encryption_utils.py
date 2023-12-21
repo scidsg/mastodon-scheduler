61c6e31d8567053b2db206bb2ce72254e20c13b7
Glenn Sorrentino <git@scidsg.org>
2023-12-20 23:14:19 -0800

    Update encryption_utils.py

--- encryption_utils.py
+++ encryption_utils.py
@@ -1,8 +1,9 @@
 from cryptography.fernet import Fernet
 
 def generate_key():
-    """Generate a new encryption key."""
-    return Fernet.generate_key()
+    key = Fernet.generate_key()
+    with open('/etc/mastodon-scheduler/encryption.key', 'wb') as key_file:
+        key_file.write(key)
 
 def encrypt_data(data, key):
     """Encrypt data using the provided key."""
@@ -13,3 +14,6 @@ def decrypt_data(encrypted_data, key):
     """Decrypt data using the provided key."""
     f = Fernet(key)
     return f.decrypt(encrypted_data).decode()
+
+# Call this function to generate and save the key
+generate_key()
