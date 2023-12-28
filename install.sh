b1900efd884b1d2c8efec39e0e0b68da1163b4f7
Glenn Sorrentino <git@scidsg.org>
2023-12-27 21:24:36 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ cat <<"EOF"
   / __/___/ /  ___ ___/ /_ __/ /__ ____  
  _\ \/ __/ _ \/ -_) _  / // / / -_) __/  
 /___/\__/_//_/\__/\_,_/\_,_/_/\__/_/                                   
-Mastodon Scheduler - Hosted v0.1.1
+Mastodon Scheduler - Hosted v0.1.2
 
 A free tool by Science & Design - https://scidsg.org
 EOF
